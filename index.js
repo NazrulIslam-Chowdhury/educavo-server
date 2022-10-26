544c2562572d183ff2732b7fc9012f2bcf3abd26
rahiislam96 <nazrulislam66188@gmail.com>
2022-10-26 12:10:50 +0600

    courses api created

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000
+const courses = require('./data/courses.json');
+const courseDetails = require('./data/details.json');
 
+app.use(cors());
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const courseId = courses.find(c => c.id == id);
+    res.send(courseId);
+})
+
+app.get('/course_details', (req, res) => {
+    res.send(courseDetails)
+})
+
+app.get('/course_details/:id', (res, req) => {
+    const id = req.params.id;
+    const detailsId = courseDetails.find(d => d.course_id === id)
+    res.send(detailsId)
+})
 
 app.get('/', (req, res) => {
     res.send('Hello world');
